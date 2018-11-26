737961150c1d86031fbb1db511e41be62753eab9
sunzhenguo <sunzhenguo@hundun.cn>
2018-11-26 17:41:43 +0800

    add extra test

---
+++ collinear/test.java
@@ -0,0 +1,14 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class test {
+    public static void main(String[] args) {
+        double a = Double.POSITIVE_INFINITY;
+        double b = Double.NEGATIVE_INFINITY;
+        if (a == b)
+            System.out.println("same");
+    }
+}
