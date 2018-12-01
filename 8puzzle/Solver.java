d86b7c4099e1752a355ad25e74b38325f68f6544
sunzhenguo_pc <1159130734@qq.com>
2018-12-01 22:52:53 +0800

    add BoardUnit--A star logic

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -13,43 +13,106 @@ import java.util.Iterator;
 
 public class Solver {
 
-    private MinPQ<Board> openSet = new MinPQ<>();
-    private MinPQ<Board> closeSet = new MinPQ<>();
+    private class BoardUnit {
+        private Board Board = null;
+        private Board Predecessor = null;
+        private int moves = 0;
+
+        public BoardUnit(Board board, int move, Board predecessor) {
+            Board = board;
+            Predecessor = predecessor;
+            moves = move;
+        }
+
+        public void setPredecessor(Board board) {
+            Predecessor = board;
+        }
+
+        public Board getPredecessor() {
+            return Predecessor;
+        }
+
+        public void setMoves(int move) {
+            moves = move;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+
+        public Board getBoard() {
+            return Board;
+        }
+    }
+
+    private MinPQ<BoardUnit> openSet = new MinPQ<>();
+    private MinPQ<BoardUnit> closeSet = new MinPQ<>();
     private int moveNumer = 0;
     private ArrayList<Board> solutionList = new ArrayList<>();
+    private boolean resolvable = false;
 
     public Solver(Board initial) {
         // find a solution to the initial board (using the A* algorithm)
-        openSet.insert(initial);
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
+        openSet.insert(new BoardUnit(initial, 0, null));
         while (!openSet.isEmpty()) {
-            Board b = openSet.min();
-            if (b.isGoal()) {
+            BoardUnit b = openSet.min();
+            if (b.getBoard().isGoal()) {
+                resolvable = true;
                 break;
             }
-            for (Board board : b.neighbors()) {
+            for (Board board : b.getBoard().neighbors()) {
                 // calc the f(board): the distance to the initial + the distance to the goal
-                int f = moveNumer + board.hamming();
+                int f = moveNumer + board.manhattan();
 
                 // check whether in the open set
+                BoardUnit bu;
                 boolean inOpen = false;
-                Iterator<Board> iter = openSet.iterator();
+                int fInOpen = 0;
+                Iterator<BoardUnit> iter = openSet.iterator();
                 while (iter.hasNext()) {
-                    if (iter.next().toString().equals(board.toString())) {
+                    bu = iter.next();
+                    if (bu.getBoard().toString().equals(board.toString())) {
                         inOpen = true;
+                        fInOpen = bu.getBoard().manhattan() + bu.getMoves();
                         break;
                     }
                 }
-                // if this neighbor in open set
-                if (inOpen) {
-                    
+                // if this neighbor in open set && new f(board) < the f(board) in the openSet;
+                if (inOpen && f < fInOpen && bu != null) {
+                    bu.setMoves(moveNumer);
+                }
+
+                //check whether in the closed set
+                boolean inClosed = false;
+                Iterator<BoardUnit> iter2 = closeSet.iterator();
+                while (iter2.hasNext()) {
+                    if (iter2.next().getBoard().toString().equals(board.toString())) {
+                        inClosed = true;
+                    }
+                }
+                // if this neighbor in closed set
+                if (inClosed) {
+                    continue;
+                }
+
+                // if not in open set and not in closed set
+                if (!inOpen && !inClosed) {
+                    BoardUnit newNode = new BoardUnit(board, moveNumer, b.getBoard());
+                    openSet.insert(newNode);
                 }
             }
+            // insert cur node in closed set
+            closeSet.insert(new BoardUnit(b.getBoard(), moveNumer, b.getPredecessor()));
         }
 
     }
 
     public boolean isSolvable() {
         // is the initial board solvable?
+        return resolvable;
     }
 
     public int moves() {
